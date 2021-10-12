7231df8f8dfbf30bee9c4dcceb5fd1d41009f78b
carlos-saldana <carlos.andres.saldana@gmail.com>
2021-10-12 00:02:29 -0500

    plataforma iot services v2

--- mycode/index.js
+++ mycode/index.js
@@ -1,7 +1,9 @@
 function hello() {
     setTimeout(() => {
-        console.log("Hello from docker node");
+        console.log("Hello from docker node v2");
         hello();
         
     }, 5000);
 }
+
+hello();
